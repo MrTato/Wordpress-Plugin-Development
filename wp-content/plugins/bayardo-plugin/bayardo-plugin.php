72c5594d7ff645ea98bb6ee581d05d8e42d28f82
Bayardo <bjlslopez@gmail.com>
2022-02-26 16:26:20 -0600

    Part 1

---
+++ wp-content/plugins/bayardo-plugin/bayardo-plugin.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package BayardoPlugin
+ */
+/*
+Plugin Name: Bayardo Plugin
+Plugin URI: http://bayardo.com
+Description: This is my first attempt on writing a custom Plugin for this amazing tutorial series.
+Version: 1.0.0
+Author: Bayardo Lopez
+Author URI: http://bayardolopez.com
+License: GPLv2 or later
+Text Domain: bayardo-plugin
+*/
